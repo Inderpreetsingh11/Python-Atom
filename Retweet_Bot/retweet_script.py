729eb7d1f7cfe52d4d708da9df1400b66999c050
Inderpreetsingh11 <inderpreetsingh4632@gmail.com>
2020-10-14 01:02:02 +0530

    Added a json file to store the id's of liked tweets,to avoid exceptions

--- Retweet_Bot/retweet_script.py
+++ Retweet_Bot/retweet_script.py
@@ -1,5 +1,7 @@
 import tweepy
 import os
+import json
+import os.path
 import services as twitter
 from dotenv import load_dotenv
 load_dotenv()
@@ -12,5 +14,23 @@ ACCESS_SECRET = os.getenv('ACCESS_SECRET')
 
 api = twitter.Authentication(API_KEY,API_SECRET,ACCESS_KEY,ACCESS_SECRET)
 
-for id in twitter.get_like_ids('Inder4632',api):
-    twitter.retweet_liked_tweets(id,api)
+likes_list = twitter.get_like_ids(api)
+
+if os.path.exists("liked_tweets_id.json") == False:
+    twitter.write_to_json(likes_list)
+    for id in likes_list:
+        twitter.retweet_liked_tweets(id,api)
+else:
+    f = open('liked_tweets_id.json',)
+    data = json.load(f)
+    for id in likes_list:
+        if id in data:
+            pass
+        else:
+            twitter.retweet_liked_tweets(id,api)
+
+    f.close()
+    with open('liked_tweets_id.json', 'w') as json_file:
+        json_file.seek(0)
+        json_file.truncate()
+        json.dump(likes_list, json_file)
--- Retweet_Bot/services.py
+++ Retweet_Bot/services.py
@@ -1,5 +1,5 @@
 import tweepy
-
+import json
 
 id_of_liked_tweets = []
 
@@ -9,7 +9,7 @@ def Authentication(API_KEY,API_SECRET,ACCESS_KEY,ACCESS_SECRET):
     api = tweepy.API(auth)
     return api
 
-def get_like_ids(screen_name,api):
+def get_like_ids(api):
     for tweet in api.favorites(screen_name = 'Inder4632'):
         id_of_liked_tweets.append(tweet.id)
     return id_of_liked_tweets
@@ -19,3 +19,7 @@ def retweet_liked_tweets(id,api):
         api.retweet(id)
     except tweepy.TweepError as e:
         print(e)
+
+def write_to_json(id_of_liked_tweets):
+    with open('liked_tweets_id.json', 'w') as json_file:
+        json.dump(id_of_liked_tweets, json_file)
